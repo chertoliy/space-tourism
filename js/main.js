9e7fb6715bb592f3ebadd31fc70bc7ef7c6beb10
Anatolii Chervontsev <104504367+chertoliy@users.noreply.github.com>
2023-08-15 00:28:30 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+    $('.slider__box').slick({
+        prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow-left.svg" alt="arrow-left">',
+        nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="arrow-right">',
+        fade: true,
+        responsive: [
+            {
+                breakpoint: 436,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
+    });
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('active');
+    });
+});
